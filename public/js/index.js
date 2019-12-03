1e3460eb0120809fda85adbaadb8bb8525ea9b85
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2019-12-03 12:19:28 +0900

    handlebars + htm + react

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+import { React, ReactDOM } from 'https://unpkg.com/es-react@16.8.60';
+import htm from 'https://unpkg.com/htm?module'
+const html = htm.bind(React.createElement);
+
+const App = () => {
+  const [ count, setCount ] = React.useState(0);
+  const [ message ] = React.useState(AppInitialState.message);
+
+  return html`
+    <div>
+      <div>message from server: ${message}</div>
+      <div>
+        <button onClick=${() => setCount(count - 1)}>-1</button>
+        [ ${count} ]
+        <button onClick=${() => setCount(count + 1)}>+1</button>
+      </div>
+    </div>
+  `;
+}
+
+ReactDOM.render(
+  html`<${App} />`,
+  document.getElementById('root')
+);
