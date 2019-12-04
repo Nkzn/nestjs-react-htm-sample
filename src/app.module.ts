cef04a4c8f7484c1c7d9d3dd267761cf0e142ec1
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2019-12-04 10:32:00 +0900

    setup session module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NestSessionOptions, SessionModule } from 'nestjs-session';
 
 @Module({
-  imports: [],
+  imports: [
+    SessionModule.forRoot({
+      session: { secret: 'hogehoge' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
